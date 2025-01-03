6e4d5b073496dcccd136612a008b404b55de1aa2
18811583798@163.com <18811583798@163.com>
2025-01-03 14:07:57 +0800

    feat: 暂停开发，先找工作

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: '/api/:path*',
+                    destination: 'http://localhost:8088/:path*'
+                }
+            ]
+        };
+    }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,71 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import "./globals.css";
 import Header from '@/app/components/header';
 import Cover from '@/app/components/cover';
+import axios from 'axios';
+import { Button, Flex, Upload, UploadProps, message } from 'antd';
+
+// 上传文件
+function filesUpload(formData: FormData) {
+  console.log('formData', formData);
+  return axios.post('/api/order/upload', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+}
+
 
 const Home = () => {
+
+  const [fileList, setFileList] = React.useState([] as any[]);
+
+  // 移除文件
+  const handleRemove = (file: any) => {
+    const files = fileList?.filter((item) => item.uid !== file?.uid);
+    setFileList(files);
+  }
+
+  // 保存选择的文件
+  const handleBeforeUpload = async (file: any, files: any[]) => {
+    console.log('files', file, files);
+    setFileList([...fileList, ...files]);
+  }
+
+  // 上传多个文件
+  const handleUploadMultiple = async () => {
+    console.log('fileList', fileList);
+    const formData = new FormData();
+    fileList.forEach((file) => {
+      formData.append('files', file);
+    });
+    await filesUpload(formData);
+    console.log(formData.keys(), 'formData');
+    message.success('上传成功');
+  }
+
+
+
   return <>
-    <div>
-      <Header />
-      <Cover />
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <Upload
+        multiple
+        fileList={fileList}
+        onRemove={handleRemove}
+        beforeUpload={handleBeforeUpload}
+      >
+        <Button>选择文件</Button>
+      </Upload>
+      <br />
+      <Button onClick={handleUploadMultiple}>点击上传多文件</Button>
+      <Button onClick={() => {
+        axios.get('/api/order/1')
+      }}>get</Button>
     </div >
   </>
 }
